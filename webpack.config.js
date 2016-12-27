7a7854dd6905c65daf085302b8f4a2171d3132ff
Florin Pop <popflorin1705@yahoo.com>
2016-12-27 11:07:37 +0200

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './app/app.jsx'
+    ],
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+            Main: 'app/components/Main.jsx',
+            applicationStyles: 'app/styles/app.css'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+};
