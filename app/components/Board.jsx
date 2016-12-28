fa37d1b6bbbb7085d8de709b356ee5119c99e25e
Florin Pop <popflorin1705@yahoo.com>
2016-12-28 22:13:55 +0200

    dynamic notes

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 import Note from 'Note';
 
 class Board extends React.Component{
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            notes: []
+        }
+    }
+    
+    addNote() {
+        let { notes } = this.state;
+        
+        notes.push(`Note ${notes.length}`);
+        
+        this.setState({
+            notes: notes
+        })
+    }
+    
     render() {
+        let { notes } = this.state;
+        
+        let displayNotes = notes.map((note, i) => {
+            return (
+                <Note key={i} noteText={note} />
+            );
+        });
+        
         return(
             <div className="board">
-                <Note text="Note 1"/>
-                <Note text="Note 2"/>
-                <Note text="Note 3"/>
-                <Note text="Note 4"/>
-                <Note text="Note 5"/>
+                { displayNotes }
+                <button onClick={this.addNote.bind(this)}>Add note</button>
             </div>
         );
     }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -5,28 +5,12 @@ class Note extends React.Component{
         super(props);
     }
     
-    getPositionStyle() {
-        let noteSize = 200; // Change if changed in CSS
-        
-        let screenH = window.innerHeight;
-        let screenW = window.innerWidth;
-        let halfNoteSize = noteSize/2;
-        let positionX = Math.floor(Math.random() * (screenW - halfNoteSize));
-        let positionY = Math.floor(Math.random() * (screenH - halfNoteSize));
-        
-        let style = {
-            left: positionX,
-            top: positionY
-        };
-        
-        return style;
-    }
-    
     render(){
+        let { noteText, left, top } = this.props.note;
         
-        let style = this.getPositionStyle();
+        let style = {...left, ...top}
         return(
-            <div className="note" style={style}>{this.props.text}</div>
+            <div className="note" style={style}>{this.props.noteText}</div>
         );
     }
 }
