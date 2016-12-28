d78cacf6eaa159d09a2e179eb8f113de92140c63
Florin Pop <popflorin1705@yahoo.com>
2016-12-28 19:24:48 +0200

    Note component

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Note from 'Note';
 
 class Board extends React.Component{
     render() {
         return(
             <div>
-                <h1>Title</h1>
+                <h1>Notes</h1>
+                <Note/>
             </div>
         );
     }
---
+++ app/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Note extends React.Component{
+    render(){
+        return(
+            <div className="note">Note</div>
+        );
+    }
+}
+
+export default Note;
