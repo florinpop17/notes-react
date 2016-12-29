93079a4f1d54ae944d2fcaf0ee46778df4c710d9
Florin Pop <popflorin1705@yahoo.com>
2016-12-29 20:01:10 +0200

    editHandle && saveHandle

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -6,6 +6,7 @@ class Note extends React.Component{
         
         this.editNote = this.editNote.bind(this);
         this.deleteNote = this.deleteNote.bind(this);
+        this.saveNote = this.saveNote.bind(this);
     }
     
     editNote() {
@@ -20,18 +21,32 @@ class Note extends React.Component{
         this.props.onDeleteNote(noteId);
     }
     
+    saveNote() {
+        
+    }
+    
     render(){
-        let { text, left, top } = this.props.note;
-         
-        return(
-            <div className="note" style={{left:left, top:top}}>
-                {text}
-                <div className="buttons">
-                    <button onClick={this.editNote}>Edit</button>
-                    <button onClick={this.deleteNote}>Delete</button>
+        let { text, left, top, editMode } = this.props.note;
+        
+        if(editMode){
+            return(
+                <div className="note" style={{left:left, top:top}}>
+                    <textarea ref="inputText" value={text}></textarea>
+                    <button onClick={this.saveNote}>Save</button>
                 </div>
-            </div>
-        );
+            );   
+        } else {
+
+            return(
+                <div className="note" style={{left:left, top:top}}>
+                    {text}
+                    <div className="buttons">
+                        <button onClick={this.editNote}>Edit</button>
+                        <button onClick={this.deleteNote}>Delete</button>
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
